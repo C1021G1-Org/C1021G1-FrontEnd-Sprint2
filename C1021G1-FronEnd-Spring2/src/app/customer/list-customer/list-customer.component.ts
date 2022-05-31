949bf410febe26ca124764bbbd9f5f270fc8a7e4
DAOBUIXUANTHANG <inorganon@gmail.com>
2022-05-31 09:26:52 +0700

    up len nhanh con

--- C1021G1-FronEnd-Spring2/src/app/customer/list-customer/list-customer.component.ts
+++ C1021G1-FronEnd-Spring2/src/app/customer/list-customer/list-customer.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Customer} from "../model/customer";
+import {CustomerService} from "../customer.service";
+import {MatDialog} from "@angular/material/dialog";
+import {DetailCustomerComponent} from "../detail-customer/detail-customer.component";
 
 @Component({
   selector: 'app-list-customer',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-customer.component.css']
 })
 export class ListCustomerComponent implements OnInit {
+  customerList : Customer[];
 
-  constructor() { }
+  constructor(private customerService : CustomerService,
+              public dialog : MatDialog) { }
 
   ngOnInit(): void {
+    this.customerService.getListCustomer(0).subscribe(data => {
+      this.customerList = data.content;
+    })
   }
 
+  openDetailCustomer(id: number) {
+    const dialogRef = this.dialog.open(DetailCustomerComponent, {
+      //ThangDBX kích thước dialog
+      width: '500px',
+      data: {datal : id}
+    })
+    dialogRef.afterClosed().subscribe(next =>{
+      this.ngOnInit();
+    })
+  }
 }
