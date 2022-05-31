5feda463ba6c6d3eee43bd427d6d8365683ae6b2
NgoBao1010 <ngoduybao1010@gmail.com>
2022-05-31 16:31:24 +0700

    push nhánh con

--- C1021G1-FronEnd-Spring2/src/app/car/model/car.ts
+++ C1021G1-FronEnd-Spring2/src/app/car/model/car.ts
@@ -1,2 +1,13 @@
+import {Customer} from "../../customer/model/customer";
+import {CarType} from "./car-type";
+
 export interface Car {
+  id:number;
+  code:string;
+  name:string;
+  carPlate:string;
+  carCompany:string;
+  delFlag:boolean;
+  customer: Customer
+  carType:CarType;
 }
