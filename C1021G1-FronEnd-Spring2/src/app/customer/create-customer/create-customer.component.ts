0f5c3fb5e6de34b77c9b5f24b328ad19419e462c
NgoBao1010 <ngoduybao1010@gmail.com>
2022-06-01 14:29:13 +0700

    pull

--- C1021G1-FronEnd-Spring2/src/app/customer/create-customer/create-customer.component.ts
+++ C1021G1-FronEnd-Spring2/src/app/customer/create-customer/create-customer.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup} from "@angular/forms";
+import {CustomerService} from "../customer.service";
+import {MatDialog} from "@angular/material/dialog";
+import {DetailCustomerComponent} from "../detail-customer/detail-customer.component";
+import {CreateCarComponent} from "../../car/create-car/create-car.component";
+import {DeleteCarComponent} from "../../car/delete-car/delete-car.component";
 
 @Component({
   selector: 'app-create-customer',
@@ -14,14 +19,32 @@ export class CreateCustomerComponent implements OnInit {
     gender: new FormControl(''),
     birthday: new FormControl(''),
     phone: new FormControl(''),
-    idWard: new FormControl(''),
+    ward: new FormControl(''),
     address: new FormControl(''),
     delFlag: new FormControl(''),
 
   })
-  constructor() { }
+  constructor(private customerService: CustomerService,
+              public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
 
+  openDialogAddCar() {
+    const dialogRef = this.dialog.open(CreateCarComponent, {
+      width: '500px',
+    })
+    dialogRef.afterClosed().subscribe(next => {
+      this.ngOnInit();
+    })
+  }
+
+  openDialogDeleteCar() {
+    const dialogRef = this.dialog.open(DeleteCarComponent, {
+      width: '500px',
+    })
+    dialogRef.afterClosed().subscribe(next => {
+      this.ngOnInit();
+    })
+  }
 }
