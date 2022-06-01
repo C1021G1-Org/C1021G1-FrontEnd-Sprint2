5338a18ca3f40b07ae32a7a4cb0e2e9475480e7e
unknown <hungson.star@gmail.com>
2022-06-01 16:34:56 +0700

    SonNH push

--- C1021G1-FronEnd-Spring2/src/app/layout/body/body.component.ts
+++ C1021G1-FronEnd-Spring2/src/app/layout/body/body.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var WOW;
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
@@ -10,6 +10,9 @@ export class BodyComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.wow();
   }
-
+  wow() {
+    new WOW().init();
+  }
 }
--- C1021G1-FronEnd-Spring2/src/app/new/news.service.ts
+++ C1021G1-FronEnd-Spring2/src/app/new/news.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
+const API_TICKET = "http://localhost:8080/api/news/"
 @Injectable({
   providedIn: 'root'
 })
 export class NewsService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+
 }
