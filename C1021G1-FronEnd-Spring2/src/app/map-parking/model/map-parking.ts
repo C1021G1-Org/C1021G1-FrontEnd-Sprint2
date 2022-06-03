413634a14118ee2ac0267f4fefd414cbb20fb533
ductinh0402 <hoangductinh0402@gmail.com>
2022-06-03 11:27:38 +0700

    tinh push

---
+++ C1021G1-FronEnd-Spring2/src/app/map-parking/model/map-parking.ts
@@ -0,0 +1,13 @@
+export interface MapParking {
+  id? : number;
+  code?: string;
+  number?: number;
+  length?: number;
+  width?: number;
+  height?: number;
+  delFlag?:boolean;
+  isEmpty?:boolean;
+  description?:string;
+  floor? :number;
+  ticket?:number;
+}
